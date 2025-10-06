1d0491bb88a1d5e2f5975301451ce2a0d86ed06b
AkimioJR <akimio.jr@gmail.com>
2025-10-06 17:17:26 +0800

    fix: correct variable name in GetVersion method for clarity

--- version.go
+++ version.go
@@ -4,10 +4,10 @@ import "net/http"
 
 // https://api.lightnovel.fun/api/smiley/get-ver
 func (c *Client) GetVersion() (uint64, error) {
-	var verionString uint64
-	err := c.doRequest(http.MethodPost, "/api/smiley/get-ver", nil, &verionString)
+	var verion uint64
+	err := c.doRequest(http.MethodPost, "/api/smiley/get-ver", nil, &verion)
 	if err != nil {
 		return 0, err
 	}
-	return verionString, nil
+	return verion, nil
 }
