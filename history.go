c712370ddd8a5137e5b221f3bf08cb44b7175f72
AkimioJR <akimio.jr@gmail.com>
2025-10-07 14:55:13 +0800

    update: modify the field order

--- history.go
+++ history.go
@@ -2,16 +2,16 @@ package lightnovel
 
 type AddHistoryRequest struct {
 	UserSecurityKey
-	FId     uint `json:"fid"`
 	ClassId uint `json:"class"`
+	FId     uint `json:"fid"`
 }
 
 // https://api.lightnovel.fun/api/history/add-history
-func (c *Client) AddHistory(fId uint, classId uint) error {
+func (c *Client) AddHistory(classId uint, fId uint) error {
 	req := AddHistoryRequest{
 		UserSecurityKey: c.credentials.UserSecurityKey,
-		FId:             fId,
 		ClassId:         classId,
+		FId:             fId,
 	}
 	_, err := doRequest[any](c, "/api/history/add-history", req)
 	return err
